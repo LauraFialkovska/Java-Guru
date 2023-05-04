17d4b722395c057afc238c43457c492aa5be6d46
Laura Fialkovska <laura.fialkovska@cdsolutions.lv>
2023-05-04 21:22:32 +0300

    Lessons 1 - 8

---
+++ src/lesson_5/level_5/ArrayDemo.java
@@ -0,0 +1,17 @@
+package lesson_5.level_5;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        Array array = new Array();
+
+        int arrayLength = array.askForNewArrayLength();
+        int[] myArray = array.create(arrayLength);
+
+        array.print(myArray);
+
+        array.fillRandomly(myArray);
+        array.print(myArray);
+
+        array.findMax(myArray);
+    }
+}
